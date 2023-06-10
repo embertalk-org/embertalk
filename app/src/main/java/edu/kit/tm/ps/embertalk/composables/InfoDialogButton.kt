e9fd7dac5eab2ccfcdca4a24208a847bac994ab8
Alexander Linder <seine@eloque.nz>
2023-06-10 10:38:54 +0200

    Add info Dialog to mac settings

---
+++ app/src/main/java/edu/kit/tm/ps/embertalk/composables/InfoDialogButton.kt
@@ -0,0 +1,39 @@
+package edu.kit.tm.ps.embertalk.composables
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Info
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+
+@Composable
+fun InfoDialogButton(
+    alertTitle: @Composable () -> Unit,
+    alertText: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val openDialog = remember { mutableStateOf(false) }
+    IconButton(onClick = {
+        openDialog.value = true
+    }) {
+        Icon(imageVector = Icons.Rounded.Info, contentDescription = "info")
+    }
+    if (openDialog.value) {
+        AlertDialog(
+            title = alertTitle,
+            text = alertText,
+            confirmButton = {
+                Button(onClick = { openDialog.value = false }) {
+                    Text("Understood")
+                }
+            },
+            onDismissRequest = { openDialog.value = false }
+        )
+    }
+}
--- app/src/main/java/edu/kit/tm/ps/embertalk/composables/SettingsView.kt
+++ app/src/main/java/edu/kit/tm/ps/embertalk/composables/SettingsView.kt
@@ -1,20 +1,25 @@
 package edu.kit.tm.ps.embertalk.composables
 
 import android.preference.PreferenceManager
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Build
 import androidx.compose.material.icons.rounded.Person
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import edu.kit.tm.ps.embertalk.R
 import edu.kit.tm.ps.embertalk.sync.MacAddressUtils
 
+@Preview
 @Composable
 fun SettingsView(
     modifier: Modifier = Modifier
@@ -22,16 +27,29 @@ fun SettingsView(
     val context = LocalContext.current
     val prefs = PreferenceManager.getDefaultSharedPreferences(context)!!
     Column(
-        modifier = Modifier.padding(10.dp)
+        modifier = Modifier.padding(10.dp),
+        verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-        SubmittableTextField(
-            label = { Text(stringResource(R.string.your_mac_address)) },
-            imageVector = Icons.Rounded.Build,
-            initialValue = prefs.getString("mac", "")!!,
-            clearOnSubmit = false,
-            inputValidator = { MacAddressUtils.isValidMacAddress(it) },
-            onSubmit = { prefs.edit().putString("mac", it.uppercase()).apply() }
-        )
+        Column {
+            SubmittableTextField(
+                label = { Text(stringResource(R.string.your_mac_address)) },
+                imageVector = Icons.Rounded.Build,
+                initialValue = prefs.getString("mac", "")!!,
+                clearOnSubmit = false,
+                inputValidator = { MacAddressUtils.isValidMacAddress(it) },
+                onSubmit = { prefs.edit().putString("mac", it.uppercase()).apply() }
+            )
+            Row {
+                InfoDialogButton(
+                    alertTitle = { Text(stringResource(R.string.mac_address_how_to)) },
+                    alertText = { Text(stringResource(R.string.mac_address_how_to_text)) },
+                )
+                Text(
+                    text = stringResource(R.string.where_to_find_mac),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+            }
+        }
         SubmittableTextField(
             label = { Text(stringResource(R.string.your_username)) },
             imageVector = Icons.Rounded.Person,
