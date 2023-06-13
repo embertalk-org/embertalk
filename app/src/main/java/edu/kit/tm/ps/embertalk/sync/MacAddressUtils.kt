77a336cca2c1c109805e7725d651462b9a905ed7
Alexander Linder <seine@eloque.nz>
2023-06-13 20:44:51 +0200

    Allow lower case mac addresses

--- app/src/main/java/edu/kit/tm/ps/embertalk/sync/MacAddressUtils.kt
+++ app/src/main/java/edu/kit/tm/ps/embertalk/sync/MacAddressUtils.kt
@@ -1,7 +1,7 @@
 package edu.kit.tm.ps.embertalk.sync
 
 object MacAddressUtils {
-    private val regex = "^([0-9A-F]{2}:){5}([0-9A-F]{2})$".toRegex()
+    private val regex = "^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$".toRegex()
 
     fun isValidMacAddress(macAddress: String): Boolean {
         return regex.matches(macAddress)
