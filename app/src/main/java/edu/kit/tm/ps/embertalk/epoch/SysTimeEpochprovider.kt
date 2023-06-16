3ad407361d973910d0ffa3f5924a5984dc447aa0
Alexander Linder <seine@eloque.nz>
2023-06-16 22:21:42 +0200

    Increase starting epoch

--- app/src/main/java/edu/kit/tm/ps/embertalk/epoch/SysTimeEpochprovider.kt
+++ app/src/main/java/edu/kit/tm/ps/embertalk/epoch/SysTimeEpochprovider.kt
@@ -3,6 +3,10 @@ package edu.kit.tm.ps.embertalk.epoch
 class SysTimeEpochprovider : EpochProvider {
 
     override fun current(): Long {
-        return System.currentTimeMillis() / 60000
+        return (System.currentTimeMillis() - START_EPOCH) / 120000
+    }
+
+    companion object {
+        private const val START_EPOCH = 1686946000000
     }
 }
