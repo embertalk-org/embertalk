8ef5f9c6ed16acbc053a0d4b65143e06d4446207
Alexander Linder <seine@eloque.nz>
2023-09-06 09:58:19 +0200

    Reset starting epoch

--- app/src/main/java/edu/kit/tm/ps/embertalk/epoch/SysTimeEpochprovider.kt
+++ app/src/main/java/edu/kit/tm/ps/embertalk/epoch/SysTimeEpochprovider.kt
@@ -3,11 +3,10 @@ package edu.kit.tm.ps.embertalk.epoch
 internal class SysTimeEpochprovider : EpochProvider {
 
     override fun current(): Long {
-        return (System.currentTimeMillis() - START_EPOCH) / EPOCH_LENGTH
+        return System.currentTimeMillis() / EPOCH_LENGTH
     }
 
     companion object {
-        private const val START_EPOCH = 1686946000000
         private const val EPOCH_LENGTH = 150 * 1000
     }
 }
