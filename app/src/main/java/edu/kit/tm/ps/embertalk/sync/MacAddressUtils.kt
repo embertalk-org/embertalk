1ba9e3dcf9278e4965d074a39a82b5f59f2dc1a9
Alexander Linder <seine@eloque.nz>
2024-07-18 00:31:06 +0200

    Remove obsolete utility class

--- app/src/main/java/edu/kit/tm/ps/embertalk/sync/MacAddressUtils.kt
+++
@@ -1,9 +0,0 @@
-package edu.kit.tm.ps.embertalk.sync
-
-object MacAddressUtils {
-    private val regex = "^([0-9A-Fa-f]{2}:){5}([0-9A-Fa-f]{2})$".toRegex()
-
-    fun isValidMacAddress(macAddress: String): Boolean {
-        return regex.matches(macAddress)
-    }
-}
